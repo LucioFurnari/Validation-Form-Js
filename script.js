1b21e24d4387e0c7b701c06b080f39869db46f5e
LucioFurnari <luciofurnari99@gmail.com>
2023-01-25 18:23:09 -0300

    Change the form submit event to handle all input valid, change password confirm to valid a flag variable

--- script.js
+++ script.js
@@ -5,9 +5,13 @@ const passwordInput = document.querySelector("#pass");
 const confirmPassInput = document.querySelector("#confPass");
 const form = document.querySelector("form");
 const overlay = document.querySelector(".overlay");
+let isPassValid = false;
 
 emailInput.addEventListener('focusout',showError)
 emailInput.addEventListener('input',showError)
+
+countryInput.addEventListener('input',showError);
+countryInput.addEventListener('focusout',showError);
 
 zipInput.addEventListener('input',showError);
 zipInput.addEventListener('focusout',showError);
@@ -22,26 +26,41 @@ function showError(event) {
   if(valid) {
     target.nextElementSibling.textContent = ``;
   } else if(typeMismatch) {
-    target.nextElementSibling.textContent = `Ingrese un ${element.type} valido.`;
+    target.nextElementSibling.textContent = `Ingrese un ${target.type} valido.`;
   } else if (tooShort) {
-    target.nextElementSibling.textContent = `El ${element.type} es muy corto, minimo 8 caracteres`;
+    target.nextElementSibling.textContent = `El ${target.type} es muy corto, minimo 8 caracteres`;
   } else if (rangeUnderflow) {
     target.nextElementSibling.textContent = `No puede ingresar números negativos`;
   } else if (patternMismatch && target["name"] === "zipCode") {
     target.nextElementSibling.textContent = `Solo puede ingresar números`;
+  } else if (patternMismatch && target["name"] === "country") {
+    target.nextElementSibling.textContent = `Ingrese un pais valido`;
   }
 }
 
 function checkPassword() {
   if(confirmPassInput.value !==  passwordInput.value) {
+    isPassValid = false;
     confirmPassInput.nextElementSibling.textContent = 'La contraseña no es igual';
   } else {
+    isPassValid = true;
     confirmPassInput.nextElementSibling.textContent = '';
   }
 }
 
 form.addEventListener('submit',(e) => {
   e.preventDefault();
+  if(emailInput.validity.valid && 
+    countryInput.validity.valid &&
+    zipInput.validity.valid && 
+    passwordInput.validity.valid &&
+    isPassValid) {
+      const message = document.querySelector(".message");
+      message.textContent = 'High Five';
+    } else {
+      const message = document.querySelector(".message");
+      message.textContent = 'Error';
+    }
   overlay.classList.add('active');
 })
 
